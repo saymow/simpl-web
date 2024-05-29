07af3b88376798eafaaf505b7ea23a0e56c47f8e
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-05-29 16:40:16 -0300

    test: ensure single character tokens are handled

--- interpreter/tests/lexer.spec.ts
+++ interpreter/tests/lexer.spec.ts
@@ -8,4 +8,20 @@ describe("Lexer)", () => {
     expect(tokens.length).toBe(1);
     expect(tokens[0].type).toBe(TokenType.EOF);
   });
+
+  it("Shoud handle single caracter tokens propery", () => {
+    const tokens = new Lexer("(){},.-+;/*").scan();
+
+    expect(tokens[0].type).toBe(TokenType.LEFT_PAREN);
+    expect(tokens[1].type).toBe(TokenType.RIGHT_PAREN);
+    expect(tokens[2].type).toBe(TokenType.LEFT_BRACE);
+    expect(tokens[3].type).toBe(TokenType.RIGHT_BRACE);
+    expect(tokens[4].type).toBe(TokenType.COMMA);
+    expect(tokens[5].type).toBe(TokenType.DOT);
+    expect(tokens[6].type).toBe(TokenType.MINUS);
+    expect(tokens[7].type).toBe(TokenType.PLUS);
+    expect(tokens[8].type).toBe(TokenType.SEMICOLON);
+    expect(tokens[9].type).toBe(TokenType.SLASH);
+    expect(tokens[10].type).toBe(TokenType.STAR);
+  });
 });
