bd003ce8bc5f548c58bf3ee2f5423c55e744c75d
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-05-29 17:29:54 -0300

    test: ensure ast binaries are handled

--- interpreter/tests/parser.spec.ts
+++ interpreter/tests/parser.spec.ts
@@ -1,7 +1,7 @@
 import Token from "../lib/token";
 import Parser from "../lib/parser";
 import TokenType from "../lib/token-type";
-import { Literal, Unary } from "../lib/expression";
+import { Binary, Literal, Unary } from "../lib/expression";
 
 describe("Parser", () => {
   it("Should handle Literals", () => {
@@ -27,7 +27,7 @@ describe("Parser", () => {
     expect(
       new Parser([
         new Token(TokenType.MINUS, "MINUS", undefined, 1),
-        new Token(TokenType.NUMBER, "true", "77", 1),
+        new Token(TokenType.NUMBER, "77", "77", 1),
         new Token(TokenType.EOF, "", undefined, 2),
       ]).parse()
     ).toEqual([
@@ -38,16 +38,140 @@ describe("Parser", () => {
     ]);
 
     expect(
-        new Parser([
-          new Token(TokenType.BANG, "BANG", undefined, 1),
-          new Token(TokenType.NUMBER, "true", "77", 1),
-          new Token(TokenType.EOF, "", undefined, 2),
-        ]).parse()
-      ).toEqual([
-        new Unary(
-          new Token(TokenType.BANG, "BANG", undefined, 1),
-          new Literal("77")
+      new Parser([
+        new Token(TokenType.BANG, "BANG", undefined, 1),
+        new Token(TokenType.NUMBER, "77", "77", 1),
+        new Token(TokenType.EOF, "", undefined, 2),
+      ]).parse()
+    ).toEqual([
+      new Unary(
+        new Token(TokenType.BANG, "BANG", undefined, 1),
+        new Literal("77")
+      ),
+    ]);
+  });
+
+  it("Should handle binaries", () => {
+    // 4 + 2 - 10
+    expect(
+      new Parser([
+        new Token(TokenType.NUMBER, "4", "4", 1),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Token(TokenType.NUMBER, "2", "2", 1),
+        new Token(TokenType.MINUS, "-", undefined, 1),
+        new Token(TokenType.NUMBER, "10", "10", 1),
+        new Token(TokenType.EOF, "", undefined, 2),
+      ]).parse()
+    ).toEqual([
+      new Binary(
+        new Binary(
+          new Literal("4"),
+          new Token(TokenType.PLUS, "+", undefined, 1),
+          new Literal("2")
         ),
-      ]);
+        new Token(TokenType.MINUS, "-", undefined, 1),
+        new Literal("10")
+      ),
+    ]);
+
+    // 4 / 2 + 10
+    expect(
+      new Parser([
+        new Token(TokenType.NUMBER, "4", "4", 1),
+        new Token(TokenType.SLASH, "/", undefined, 1),
+        new Token(TokenType.NUMBER, "2", "2", 1),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Token(TokenType.NUMBER, "10", "10", 1),
+        new Token(TokenType.EOF, "", undefined, 2),
+      ]).parse()
+    ).toEqual([
+      new Binary(
+        new Binary(
+          new Literal("4"),
+          new Token(TokenType.SLASH, "/", undefined, 1),
+          new Literal("2")
+        ),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Literal("10")
+      ),
+    ]);
+
+    // 10 + 4 / 2
+    expect(
+      new Parser([
+        new Token(TokenType.NUMBER, "10", "10", 1),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Token(TokenType.NUMBER, "4", "4", 1),
+        new Token(TokenType.STAR, "*", undefined, 1),
+        new Token(TokenType.NUMBER, "2", "2", 1),
+        new Token(TokenType.EOF, "", undefined, 2),
+      ]).parse()
+    ).toEqual([
+      new Binary(
+        new Literal("10"),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Binary(
+          new Literal("4"),
+          new Token(TokenType.STAR, "*", undefined, 1),
+          new Literal("2")
+        )
+      ),
+    ]);
+
+    // 4 + 2 - 10 >= 50
+    expect(
+      new Parser([
+        new Token(TokenType.NUMBER, "4", "4", 1),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Token(TokenType.NUMBER, "2", "2", 1),
+        new Token(TokenType.MINUS, "-", undefined, 1),
+        new Token(TokenType.NUMBER, "10", "10", 1),
+        new Token(TokenType.GREATER_EQUAL, ">=", undefined, 1),
+        new Token(TokenType.NUMBER, "50", "50", 1),
+        new Token(TokenType.EOF, "", undefined, 2),
+      ]).parse()
+    ).toEqual([
+      new Binary(
+        new Binary(
+          new Binary(
+            new Literal("4"),
+            new Token(TokenType.PLUS, "+", undefined, 1),
+            new Literal("2")
+          ),
+          new Token(TokenType.MINUS, "-", undefined, 1),
+          new Literal("10")
+        ),
+        new Token(TokenType.GREATER_EQUAL, ">=", undefined, 1),
+        new Literal("50")
+      ),
+    ]);
+
+    // 50 == 4 + 2 - 10
+    expect(
+      new Parser([
+        new Token(TokenType.NUMBER, "50", "50", 1),
+        new Token(TokenType.LESS_EQUAL, "<=", undefined, 1),
+        new Token(TokenType.NUMBER, "4", "4", 1),
+        new Token(TokenType.PLUS, "+", undefined, 1),
+        new Token(TokenType.NUMBER, "2", "2", 1),
+        new Token(TokenType.MINUS, "-", undefined, 1),
+        new Token(TokenType.NUMBER, "10", "10", 1),
+        new Token(TokenType.EOF, "", undefined, 2),
+      ]).parse()
+    ).toEqual([
+      new Binary(
+        new Literal("50"),
+        new Token(TokenType.LESS_EQUAL, "<=", undefined, 1),
+        new Binary(
+          new Binary(
+            new Literal("4"),
+            new Token(TokenType.PLUS, "+", undefined, 1),
+            new Literal("2")
+          ),
+          new Token(TokenType.MINUS, "-", undefined, 1),
+          new Literal("10")
+        )
+      ),
+    ]);
   });
 });
