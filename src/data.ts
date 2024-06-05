a0861968d36d6860104b6046680a12cebf294172
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-06-05 19:39:30 -0300

    feat: update initial program

--- src/data.ts
+++ src/data.ts
@@ -1,36 +1,71 @@
-export const INITIAL_PROGRAM = `var a_x;
-var a_y;
-var b_x;
-var b_y;
-var slope;
-var y_intercept;
+export const INITIAL_PROGRAM = `//        _______ __                         __ 
+//        |          __|__|.----------.-----.|    |
+//        |__          |    ||              |  _   ||    |
+//        |_______|__||__|__|__|   __||__|
+//                                      |__|                                                     
+//                                        
+//        ____           _            __      _         _   
+//       |_    _|___ _|   |___    |    |     |_|___|   |_
+//          |  |  |   .  |  .   |   .  |   |    |__ |  |_   -|    _|
+//          |_|  |___|___|___|   |_____|_|___|__|  
+//
+//                        
+//  https://github.com/saymow/simpl
 
-fun compute_slope(x0, y0, x, y) {
-    return (y - y0) / (x - x0);
+var running = true;
+var items = [];
+
+fun addItem(name) {
+    push(items, { name: name });
 }
 
-fun compute_y_intercept(x, y, slope) {
-    return y - x * slope;
+fun removeItem(name) {
+   var newItems = [];
+   
+   for (var idx = 0; idx < len(items); idx++) {
+        if (items[idx].name != name) {
+            unshift(newItems, items[idx]);
+        }
+   }
+
+   items = newItems;
 }
 
-output("1° point x: ");
-a_x = number(input());
-output("1° point y: ");
-a_y = number(input());
-output("2° point x: ");
-b_x = number(input());
-output("2° point y: ");
-b_y = number(input());
-
-slope = compute_slope(a_x, a_y, b_x, b_y);
-y_intercept = compute_y_intercept(a_x, a_y, slope);
-
-if (y_intercept  > 0) {
-    output("y = " + string(slope) + "x + " + string(y_intercept));
-} else if (y_intercept < 0) {
-    output("y = " + string(slope) + "x - " + string(abs(y_intercept)));
-} else {
-    output("y = " + string(slope) + "x");
+fun listItems() {
+   output("Items: ");
+ 
+   for (var idx = 0; idx < len(items); idx++) {
+        output(" " + string(idx + 1) + ") " + items[idx].name);
+   }
 }
 
+while (running) {
+    clear();
+    output("Select one option: ");
+    output("  1)  Add item");
+    output("  2)  Remove item");
+    output("  3)  List items");
+    output("  4)  Exit");
+    
+    var option = int(input());
+    clear();
+
+    if (option == 1) {
+        output("Item name: ");
+        addItem(input());
+    } else if (option == 2) {
+        output("Item name: "); 
+        removeItem(input());
+    } else if (option == 3) {
+        listItems();
+        sleep(2000); 
+    } else if (option == 4) {
+        running = false;   
+    } else {
+        output("Invalid option.");
+    }
+}
+
+output("END...");
 `;
+
