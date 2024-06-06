86d6e32bcf2b2e497ac84ea09492e59ac38dc255
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-06-06 12:50:24 -0300

    fix: bfs snippet

--- playground/src/snippets/bredth-first-search.ts
+++ playground/src/snippets/bredth-first-search.ts
@@ -3,99 +3,106 @@ import HEADER_SNIPPET from "./header";
 export const BREADTH_FIRST_SEARCH_SNIPPET = `${HEADER_SNIPPET}
 
 var elements = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "FINAL",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R"
-  ];
-var matrix = [
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "FINAL",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R"
 ];
-
+var matrix = [
+  [0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+  [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+  [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+  [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+var origin;
+var target;
 
 fun fill(len, value) {
-  var arr = [];
-  
-  for (var i = 0; i < len; i++) {
-     push(arr, value);
-  }
+var arr = [];
 
-  return arr;
+for (var i = 0; i < len; i++) {
+   push(arr, value);
+}
+
+return arr;
 }
 
 fun path(order, elements, originIdx, targetIdx) {
-  var idx = targetIdx;
-  var path = [];
+var idx = targetIdx;
+var path = [];
 
-  while (order[idx] != -1) {
-    unshift(path, elements[idx]);
-    idx = order[idx];
-  }
+while (order[idx] != -1) {
+  unshift(path, elements[idx]);
+  idx = order[idx];
+}
 
-  unshift(path, elements[originIdx]);
+unshift(path, elements[originIdx]);
 
-  return path;
+return path;
 }
 
 fun bfs(matrix, elements, origin, target) {
-  var originIdx = indexOf(elements, origin);
-  var targetIdx = indexOf(elements, target);
-  var visited = fill(len(elements), 0);
-  var order = fill(len(elements), -1);
-  var queue = [originIdx];
+var originIdx = indexOf(elements, origin);
+var targetIdx = indexOf(elements, target);
+var visited = fill(len(elements), 0);
+var order = fill(len(elements), -1);
+var queue = [originIdx];
 
-  while (len(queue) > 0) {
-    var itemIdx = pop(queue);
-    
-    if (itemIdx == targetIdx) return path(order, elements, originIdx, targetIdx);
+while (len(queue) > 0) {
+  var itemIdx = pop(queue);
+  
+  if (itemIdx == targetIdx) return path(order, elements, originIdx, targetIdx);
 
-    for (var idx = 0; idx < len(elements); idx++) {
-      if (matrix[itemIdx][idx] == 1 and visited[idx] == 0) {
-        order[idx] = itemIdx; 
-        unshift(queue, idx);
-      }
+  for (var idx = 0; idx < len(elements); idx++) {
+    if (matrix[itemIdx][idx] == 1 and visited[idx] == 0) {
+      order[idx] = itemIdx; 
+      unshift(queue, idx);
     }
-    
-    visited[itemIdx] = 1;
   }
+  
+  visited[itemIdx] = 1;
+}
 
-  return nil;
+return nil;
 }
 
 output("Select 2 elements: ");
 
-print bfs(matrix, elements, input("Origem: "), input("Target: "));
+output("Origin: ");
+origin = input();
+
+output("Target: ");
+target = input();
+
+print bfs(matrix, elements, origin, target);
 `;
